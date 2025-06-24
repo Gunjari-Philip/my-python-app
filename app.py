e742f15ad259857e1f0fc563eb4cbad4afaec089
Gunjari-Philip <philipgunjari@gmail.com>
2025-06-24 11:35:25 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from Dockerized Python App!"
+def greet(name):
+    return f"Hello, {name}! This is running inside Docker."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    name = input("Enter your name: ")
+    print(greet(name))
+
